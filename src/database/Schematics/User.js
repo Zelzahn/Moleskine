bea985bdef4f058d37544aaf0df42e48f0162ce2
Friedrich <friedrich@friedrichvdb.be>
2021-03-12 11:16:33 +0100

    db cleanup

--- src/database/Schematics/User.js
+++ src/database/Schematics/User.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const reqString = {
-	type: String,
-	required: true,
+  type: String,
+  required: true,
 };
 
 const userSchema = mongoose.Schema({
-	gulidId: reqString,
-	userId: reqString,
-	points: Number,
-	streak: Number,
-	bets: Array
+  gulidId: reqString,
+  userId: reqString,
+  points: Number,
+  bets: Array,
 });
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);

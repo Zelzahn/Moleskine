dafeaae7b95c16de08a4c618f84d28b882fa02aa
Daan Wettinck <Daan.Wettinck@UGent.be>
2021-03-12 16:34:40 +0100

    Added Config in DB

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
-import { mongoDB } from "./../config.json";
-
-module.exports = async () => {
-  await mongoose.connect(mongoDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+import { mongoDB } from "../../config.json";
+import Config from "./Schematics/Config"
+import { Candidate, User, MoleBet, Bet } from "./Schematics/User"
+import { logger } from "../index";
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).then(() => logger.log("info", "Connected to DB"))
+  .catch(err => {
+    logger.log("error", `Cannot connect to DB: ${err}`);
   });
-  return mongoose;
-};
+module.exports.getSetting = async (setting) => {
+  return await Config.findOne({ "name": setting })
+}
+module.exports.getWeek = async () => {
+  return await Config.findOne({ "name": "week" })
+}
+
+module.exports.setSetting = async (setting, value) => {
+  Config.update({ "name": setting }, { "value": value }, { upsert: true });
+}

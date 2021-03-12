b4a0fa0fe916bd87a45a68d227ebaf771538d82e
Friedrich <friedrich@friedrichvdb.be>
2021-03-12 11:49:50 +0100

    command outline

--- src/commands/general/vote.js
+++ src/commands/general/vote.js
@@ -1,5 +1,6 @@
 import { Command } from "discord.js-commando";
 import { participants } from "../../../config.json";
+import { MessageEmbed } from "discord.js";
 
 export default class VoteCommand extends Command {
   constructor(client) {
@@ -12,10 +13,6 @@ export default class VoteCommand extends Command {
     });
   }
 
-  // Stuur "Wie denk je dat er allemaal de aflevering zal overleven?"
-  // 1. naam:bedrag naam:bedrag etc.
-  // Stuur "Wie is de mol?"
-  // 2. naam
   async run(message) {
     // TODO: Logger deftig in een aparte file doen
     console.log(`${message.author.username} has used vote.`);
@@ -34,19 +31,24 @@ export default class VoteCommand extends Command {
         time: 30000,
       })
       .catch(() => {
-        message.say("Timeout: You waited too long to respond.");
+        throw new Error("Timeout: You waited too long to respond.");
       });
-
-    collected.first().react("✅");
 
     const persons = collected
       .first()
       .content.split(" ")
       .map((person) => person.split(":"));
 
-    console.log(persons.every((p) => participants.includes(p[0])));
+    if (!persons.every((p) => participants.includes(p[0])))
+      throw new Error("Not every person is a participant");
 
-    message.say("Wie is de mol?");
+    collected.first().react("✅");
+
+    const moleEmbded = new MessageEmbed({ title: "Wie is de mol?" });
+    for (let [index, person] of participants.entries()) {
+      moleEmbded.addField(index, person);
+    }
+    message.embed(moleEmbded);
 
     collected = await message.channel
       .awaitMessages(filter, {
@@ -55,12 +57,19 @@ export default class VoteCommand extends Command {
         errors: ["time"],
       })
       .catch(() => {
-        message.say("Timeout: You waited too long to respond.");
-        // TODO: De log hieronder wordt lijk ook nog uitgevoerd?
+        throw new Error("Timeout: You waited too long to respond.");
       });
 
     collected.first().react("✅");
 
     console.log(collected.first().content);
+  }
+
+  onError(err, message) {
+    message.embed({
+      title: "Error",
+      description: err.message,
+      color: "#FF2400",
+    });
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import { join } from "path";
 
 import { prefix, ownerid, token } from "../config.json";
 
+const logger = winston.createLogger({
+  transports: [
+    // new winston.transports.Console(),
+    new winston.transports.File({ filename: "log" }),
+  ],
+  format: winston.format.printf(
+    (log) => `[${log.level.toUpperCase()}] - ${log.message}`
+  ),
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(new winston.transports.Console());
+}
+
 const client = new CommandoClient({
   commandPrefix: prefix,
   owner: ownerid,
@@ -14,17 +28,21 @@ client.registry
   .registerDefaultTypes()
   .registerDefaultGroups()
   .registerGroups([["general", "General commands"]])
+  .registerGroups([["management", "Commands to manage the bot"]])
   .registerDefaultCommands()
   .registerCommandsIn(join(__dirname, "commands"));
 
 client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+  logger.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
   client.user.setPresence({
     activity: { name: "for help", type: "WATCHING" },
     status: "online",
   });
 });
 
-client.on("error", console.error);
+client.on("warn", (m) => logger.log("warn", m));
+client.on("error", (m) => logger.log("error", m));
+
+process.on("uncaughtException", (error) => logger.log("error", error));
 
 client.login(token);
